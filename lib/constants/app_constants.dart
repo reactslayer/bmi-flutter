3ed5d6d1ca4acbea40e8b9b86832a368d2979e06
Ninad Apte <ninadapte12345@gmail.com>
2023-03-11 16:09:17 +0530

    Finall

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+// ignore: import_of_legacy_library_into_null_safe
+
+final Color mainHexColor = Colors.black12;
+final Color accentHexColor = Colors.yellowAccent;
